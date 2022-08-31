6315dbb8370ca65d6eb4351e9f92e8372892e085
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-31 14:58:06 -0500

    changed the classification showed based on the expected and unexpected values the user could input

--- src/main/kotlin/com/rroperations/services/TrainServiceImpl.kt
+++ src/main/kotlin/com/rroperations/services/TrainServiceImpl.kt
@@ -3,24 +3,39 @@ package com.example.railRoadOperations.services
 import com.example.railRoadOperations.models.ClassificationTrack
 import com.example.railRoadOperations.models.Train
 import com.example.railRoadOperations.utils.TrainComparator
+import com.rroperations.models.DestinationsOrder
+import com.rroperations.models.ReceiversOrder
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
 @Singleton
-class TrainServiceImpl: TrainServiceInterface {
+class TrainServiceImpl(
+    private val destinationsOrder: DestinationsOrder,
+    private val receiversOrder: ReceiversOrder): TrainServiceInterface {
 
     @Inject
     lateinit var comparator: TrainComparator
 
     override fun orderTrains(trains: ArrayList<Train>): List<ClassificationTrack> {
+
+        val destinations = destinationsOrder.destinations
+        val receivers = receiversOrder.receivers
+
         val response: ArrayList<ClassificationTrack> = ArrayList()
+
         val trainsSorted = comparator.sortTrains(trains)
-        trainsSorted.forEachIndexed{index, train ->
-            val classificationTrack = ClassificationTrack(index + 1, train.nameOfCar, train.destination, train.receiver)
+        trainsSorted.map{ train ->
+            val classificationTrack = ClassificationTrack(
+                if (destinations.containsKey(train.destination) && receivers.containsKey(train.receiver))
+                destinations[train.destination].toString() else "DLQ",
+                train.nameOfCar,
+                train.destination,
+                train.receiver)
             response.add(classificationTrack)
         }
         println(response)
         println(trainsSorted)
+
         return response
     }
 
