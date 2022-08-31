c789aea9719643a9887179f7c34ed03945f92c29
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-31 14:56:29 -0500

    Map with the receivers order of priority

--- src/main/kotlin/com/rroperations/models/ReceiversOrder.kt
+++ src/main/kotlin/com/rroperations/models/ReceiversOrder.kt
@@ -1,4 +1,12 @@
 package com.rroperations.models
 
+import jakarta.inject.Singleton
+
+@Singleton
 class ReceiversOrder {
+     val receivers = mapOf(
+        "UPS" to 1,
+        "FedEx" to 2,
+        "Old Dominion" to 3
+    )
 }
