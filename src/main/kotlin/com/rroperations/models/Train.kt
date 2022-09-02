caba74dba8973ddb323f60f319b7977bb278ab05
eduardoesparza <eduebernal@gmail.com>
2022-09-02 12:38:17 -0700

    add ClassificationTrack and trainId attributes to Train class

--- src/main/kotlin/com/rroperations/models/Train.kt
+++ src/main/kotlin/com/rroperations/models/Train.kt
@@ -1,9 +1,13 @@
 package com.rroperations.models
 
+import java.util.UUID
+
 data class Train(
         val nameOfCar: String,
         val destination: String,
-        val receiver: String) {
+        val receiver: String,
+        var classificationTrack: String? = null,
+        var trainId: UUID? = null) {
 
     override fun toString(): String {
         return "Train(nameOfCar='$nameOfCar', destination='$destination', receiver='$receiver')"
