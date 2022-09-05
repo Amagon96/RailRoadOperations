5d0a1470634d71fcd31b3da6c96385ebbb12bb85
Eduardo Esparza <92958867+eduebernal@users.noreply.github.com>
2022-09-05 07:31:46 -0700

    remove trainId attribute

--- src/main/kotlin/com/rroperations/models/TrainCar.kt
+++ src/main/kotlin/com/rroperations/models/TrainCar.kt
@@ -1,10 +1,7 @@
 package com.rroperations.models
 
-import java.util.UUID
-
 data class TrainCar(
         val nameOfCar: String,
         val destination: String,
         val receiver: String,
-        var classificationTrack: String? = null,
-        var trainId: UUID? = null)
+        var classificationTrack: String? = null)
