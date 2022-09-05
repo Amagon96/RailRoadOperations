27dd82d2293d69955c492c98526e48eea035e933
Eduardo Esparza <92958867+eduebernal@users.noreply.github.com>
2022-09-05 07:42:46 -0700

    change nameOfCar to name in TrainCar class

--- src/main/kotlin/com/rroperations/models/TrainCar.kt
+++ src/main/kotlin/com/rroperations/models/TrainCar.kt
@@ -1,7 +1,7 @@
 package com.rroperations.models
 
 data class TrainCar(
-        val nameOfCar: String,
+        val name: String,
         val destination: String,
         val receiver: String,
         var classificationTrack: String? = null)
